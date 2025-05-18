6f04c236fcd2f96fc2be4a7e3dbab23a65fc0820
foreverfl <forever_fl@naver.com>
2025-05-18 22:55:42 +0900

    feat: gateway 내 /auth/refresh 구현 완료료

--- apps/gateway/src/auth/controller.ts
+++ apps/gateway/src/auth/controller.ts
@@ -1,7 +1,11 @@
 import { AuthService } from "@/auth/service";
 import { Public } from "@/common/decorator/public.decorator";
-import { LoginRequestDto, RegisterRequestDto } from "@/common/dto/auth.dto";
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import {
+  LoginRequestDto,
+  RefreshTokenRequestDto,
+  RegisterRequestDto,
+} from "@/common/dto/auth.dto";
+import { Body, Controller, Post } from "@nestjs/common";
 
 @Controller("auth")
 @Public()
@@ -20,32 +24,14 @@ export class AuthController {
 
   @Post("logout")
   logout() {
-    return { success: true, message: "Stub: logout processed" };
+    return {
+      success: false,
+      message: "Logout functionality has not been implemented yet.",
+    };
   }
 
   @Post("refresh")
-  refresh() {
-    return {
-      accessToken: "new-stub-access-token",
-      refreshToken: "new-stub-refresh-token",
-    };
-  }
-
-  @Get("me")
-  getMe() {
-    // 보통 req.user에서 꺼냄 (Guard 붙인 후)
-    return {
-      userId: "stub-user-id",
-      email: "stub@example.com",
-      role: "USER",
-    };
-  }
-
-  @Get("role/:userId")
-  getRole(@Param("userId") userId: string) {
-    return {
-      userId,
-      role: "USER", // Stub 용으로 고정
-    };
+  refresh(@Body() dto: RefreshTokenRequestDto) {
+    return this.authService.refreshTokens(dto);
   }
 }
