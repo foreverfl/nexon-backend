41cf535a32da919237c490b96376e5ff91f3338f
foreverfl <forever_fl@naver.com>
2025-05-18 14:55:33 +0900

    feat: Redis 및 Kafka 연동 추가가

--- apps/auth/src/app.module.ts
+++ apps/auth/src/app.module.ts
@@ -1,8 +1,19 @@
 import { AuthModule } from "@/auth/module";
+import { RedisModule } from "@nestjs-modules/ioredis";
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI || ""), AuthModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI || ""),
+    RedisModule.forRoot({
+      type: "single",
+      options: {
+        host: process.env.REDIS_HOST || "localhost",
+        port: parseInt(process.env.REDIS_PORT || "6379"),
+      },
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
