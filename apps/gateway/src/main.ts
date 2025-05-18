ee7c9ebde2b1c1e6fcf2d0458acaa9df0903eadd
foreverfl <forever_fl@naver.com>
2025-05-19 00:40:53 +0900

    feat: JWT 토큰 저장용 Swagger 추가가

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -4,12 +4,28 @@ import { HttpLoggingInterceptor } from "@/common/interceptor/logging.interceptor
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory, Reflector } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle("API Gateway")
+    .setDescription("MSA 구조 Gateway 서버 Swagger 문서")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("swagger", app, document);
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  // 로깅 인터셉터 설정
   app.useGlobalInterceptors(new HttpLoggingInterceptor());
+
+  // JWT 인증 및 권한 설정
   const reflector = app.get(Reflector);
   const rolesGuard = new RolesGuard(reflector);
   const jwtAuthGuard = app.get(JwtAuthGuard);
