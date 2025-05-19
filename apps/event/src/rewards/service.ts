0823ecc8176035f1abe826e6269678c8ba587e8e
foreverfl <forever_fl@naver.com>
2025-05-20 00:31:48 +0900

    feat: event 내 rewards 관련 엔드포인트 구현 완료

--- apps/event/src/rewards/service.ts
+++ apps/event/src/rewards/service.ts
@@ -1,6 +1,15 @@
 import {
   CreateRewardRequestDto,
   CreateRewardResponseDto,
+  DeleteRewardRequestDto,
+  DeleteRewardResponseDto,
+  GetAllRewardsRequestDto,
+  GetAllRewardsResponseDto,
+  GetRewardByIdRequestDto,
+  GetRewardByIdResponseDto,
+  RewardDto,
+  UpdateRewardRequestDto,
+  UpdateRewardResponseDto,
 } from "@/common/dto/requests.dto";
 import { Injectable } from "@nestjs/common";
 import { Types } from "mongoose";
@@ -14,5 +23,55 @@ export class RewardsService {
   ): Promise<CreateRewardResponseDto> {
     const reward = await this.rewardsRepository.create(dto);
     return { id: (reward._id as Types.ObjectId).toString() };
+  }
+
+  async getAllRewards(
+    dto: GetAllRewardsRequestDto,
+  ): Promise<GetAllRewardsResponseDto> {
+    const { rewards, total, page, limit } =
+      await this.rewardsRepository.findAll(dto);
+    return {
+      rewards: rewards.map(
+        (r) =>
+          new RewardDto({
+            ...r,
+            id: (r._id as Types.ObjectId).toString(),
+          }),
+      ),
+      total,
+      page,
+      limit,
+    };
+  }
+
+  async getRewardById(
+    dto: GetRewardByIdRequestDto,
+  ): Promise<GetRewardByIdResponseDto> {
+    const reward = await this.rewardsRepository.findById(dto.id);
+    return {
+      reward: new RewardDto({
+        ...reward,
+        id: (reward._id as Types.ObjectId).toString(),
+      }),
+    };
+  }
+
+  async updateReward(
+    dto: UpdateRewardRequestDto,
+  ): Promise<UpdateRewardResponseDto> {
+    const reward = await this.rewardsRepository.update(dto);
+    return {
+      reward: new RewardDto({
+        ...reward,
+        id: (reward._id as Types.ObjectId).toString(),
+      }),
+    };
+  }
+
+  async deleteReward(
+    dto: DeleteRewardRequestDto,
+  ): Promise<DeleteRewardResponseDto> {
+    await this.rewardsRepository.softDelete(dto.id);
+    return { success: true };
   }
 }
