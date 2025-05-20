729ac28ec0c2f7061e080990b555da8240889b03
foreverfl <forever_fl@naver.com>
2025-05-20 11:33:17 +0900

    feat: gateway 내 reward-requets 엔드포인트 event 서버 연결

--- apps/gateway/src/reward-requests/service.ts
+++ apps/gateway/src/reward-requests/service.ts
@@ -1,8 +1,27 @@
+import { GrpcRewardRequestsService } from "@/reward-requests/grpc-reward-requests.service";
+import {
+  GetAllRewardRequestsRequestDto,
+  RequestRewardRequestDto,
+} from "@/reward-requests/requests.dto";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class RewardRequestsService {
-  test() {
-    return { message: "Rewards service is running 🎉" };
+  constructor(private readonly grpc: GrpcRewardRequestsService) {}
+
+  requestReward(dto: RequestRewardRequestDto) {
+    return this.grpc.requestReward(dto);
+  }
+
+  getMyRewardRequests(userId: string) {
+    return this.grpc.getMyRewardRequests({ userId });
+  }
+
+  getAllRewardRequests(dto: GetAllRewardRequestsRequestDto) {
+    return this.grpc.getAllRewardRequests(dto);
+  }
+
+  getRewardRequestById(id: string) {
+    return this.grpc.getRewardRequestById({ id });
   }
 }
