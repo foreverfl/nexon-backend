37c1c4cdb05be6f26b3cac91bc44636253e69d68
foreverfl <forever_fl@naver.com>
2025-05-20 14:53:08 +0900

    feat: 이벤트 보상 조건 검사 추가

--- apps/event/src/reward-requests/module.ts
+++ apps/event/src/reward-requests/module.ts
@@ -1,10 +1,14 @@
+import { Event, EventSchema } from "@/common/schema/events.schema"; // ✅ 수정됨
 import {
   RewardRequest,
   RewardRequestSchema,
 } from "@/common/schema/reward-requests.schema";
+import { Reward, RewardSchema } from "@/common/schema/rewards.schema";
+import { EventsRepository } from "@/events/repository";
 import { RewardRequestsController } from "@/reward-requests/controller";
 import { RewardRequestsRepository } from "@/reward-requests/repository";
 import { RewardRequestsService } from "@/reward-requests/service";
+import { RewardsRepository } from "@/rewards/repository";
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 
@@ -12,9 +16,16 @@ import { MongooseModule } from "@nestjs/mongoose";
   imports: [
     MongooseModule.forFeature([
       { name: RewardRequest.name, schema: RewardRequestSchema },
+      { name: Reward.name, schema: RewardSchema },
+      { name: Event.name, schema: EventSchema },
     ]),
   ],
   controllers: [RewardRequestsController],
-  providers: [RewardRequestsService, RewardRequestsRepository],
+  providers: [
+    RewardRequestsService,
+    RewardRequestsRepository,
+    EventsRepository,
+    RewardsRepository,
+  ],
 })
 export class RewardRequestsModule {}
