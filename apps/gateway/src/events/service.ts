3f018a6b60480b0092b6a1b6b769326d4d163f70
foreverfl <forever_fl@naver.com>
2025-05-20 11:08:35 +0900

    feat: gateway 내 events 엔드포인트 event 서버 연결

--- apps/gateway/src/events/service.ts
+++ apps/gateway/src/events/service.ts
@@ -1,8 +1,32 @@
+import {
+  CreateEventRequestDto,
+  GetAllEventsRequestDto,
+  UpdateEventRequestDto,
+} from "@/events/events.dto";
 import { Injectable } from "@nestjs/common";
+import { GrpcEventsService } from "./grpc-events.service";
 
 @Injectable()
 export class EventsService {
-  test() {
-    return { message: "Events service is running 🎉" };
+  constructor(private readonly grpc: GrpcEventsService) {}
+
+  createEvent(dto: CreateEventRequestDto) {
+    return this.grpc.createEvent(dto);
+  }
+
+  getAllEvents(dto: GetAllEventsRequestDto) {
+    return this.grpc.getAllEvents(dto);
+  }
+
+  getEventById(id: string) {
+    return this.grpc.getEventById({ id });
+  }
+
+  updateEvent(id: string, dto: UpdateEventRequestDto) {
+    return this.grpc.updateEvent({ ...dto, id });
+  }
+
+  deleteEvent(id: string) {
+    return this.grpc.deleteEvent({ id });
   }
 }
